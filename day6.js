519bee65f7d49cf0757d544ffcdc9dc09dad99a4
David Griswold <novachild@gmail.com>
2023-12-06 09:19:20 -0300

    day 6 explicit way added

--- day6.js
+++ day6.js
@@ -11,6 +11,7 @@ let records = lines[1].split(/ +/g).slice(1).map(Number);
 // math: t(time - t) > r finds the first value that beats the record
 // then there are time - 2t + 1 total values that break it
 let winTimes = [];
+let winTimesExplicit = [];
 for (let i = 0; i < times.length; i++) {
     let t = 0;
     while (true) {
@@ -18,12 +19,15 @@ for (let i = 0; i < times.length; i++) {
         if (t >= times[i]/2) break; //no solution
         t++;
     }
+    let x = times[i]/2 - Math.sqrt(times[i]/2 * times[i]/2 - records[i]);
+    winTimesExplicit.push(times[i]-2*Math.ceil(x) + 1);
     winTimes.push(times[i]-2*t+1);
 
 }
-console.log(winTimes);
+console.log("Loop way");
 console.log(winTimes.reduce((a,b)=>a*b,1));
-
+console.log("Explicit way");
+console.log(winTimesExplicit.reduce((a,b)=>a*b));
 let time2 = Number(lines[0].split(/ +/g).slice(1).join(""));
 let record2 = Number(lines[1].split(/ +/g).slice(1).join(""));
 console.log(time2,record2);
@@ -32,6 +36,12 @@ let t = 0;
     while (true) {
         if (t * (time2-t) > record2) break;
         if (t >= time2/2) break; //no solution
-        t++;
+        t++;  
     }
+    console.log("Loop way part 2");
 console.log(time2 - 2*t + 1);
+console.log("Explicit way part 2");
+let x = time2/2 - Math.sqrt(time2/2 * time2/2 - record2);
+console.log(time2 - 2 * Math.ceil(x) + 1);
+
+// faster direct calculation way
